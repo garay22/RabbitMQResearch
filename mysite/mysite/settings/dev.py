36ff936b2c223055548fd7fd79fd6699790ca569
Griffin Ray <griffinaray19@gmail.com>
2023-02-18 22:26:52 -0600

    Preliminary Wagtail About page

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-j16j-0amou%93=y4e8p0k4)fzl#t^02^61cnp%cerxf#by@*8^"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
